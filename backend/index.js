ac3560f1fc891195d366269f1cf4f7242511fd32
unknown <oluwatoyosiolaniyan@gmail.com>
2024-04-10 04:32:56 -0700

    vercel.json added

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ const path = require("path");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 //Database Connection with Mongo
 
